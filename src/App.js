0af96754a4db889704dad3adcbd9405541bddf80
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-04-22 22:08:09 -0400

    Add routes using react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home";
+import TripList from "./components/trips/TripList";
+import TripDetails from "./components/trips/TripDetails";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/trips" component={TripList} />
+        <Route path="/tripdetails/:phoneId" component={TripDetails} />
       </Switch>
     </div>
   );
