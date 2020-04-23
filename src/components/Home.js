635b4632df119798769d43a7988f7ca9eadfa63b
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-04-22 22:09:02 -0400

    Update what displays on Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import TripList from "./trips/TripList";
+import { Route, Switch, Link } from "react-router-dom";
+import AddTrip from "./trips/AddTrip";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Your Upcoming Trips:</h1>
+        <h1>Plan your next trip!</h1>
+        <AddTrip />
         <hr />
-        <TripList />
+        <Link to="/trips">View Your Trips</Link>
       </div>
     );
   }
