01e295251088151a7df437846da54143dcefa702
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-04-25 20:35:42 -0400

    Create Navbar file and AppBar component

---
+++ src/components/Navbar/ButtonAppBar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import "typeface-roboto";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h3" className={classes.title}>
+            Trip Planner
+          </Typography>
+          <Link to="/Login">
+            <Button variant="outlined">Login</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
