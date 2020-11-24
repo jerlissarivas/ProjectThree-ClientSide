193aa3fdf0999e1764ab4203b0ba0c02de4aee24
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-11-23 21:28:22 -0500

    Update add trip routes

--- src/components/trips/AddTrip.js
+++ src/components/trips/AddTrip.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./Trips.css";
-import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Button from "@material-ui/core/Button";
 
 class AddTrip extends Component {
   state = {
